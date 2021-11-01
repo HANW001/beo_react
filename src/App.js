20f274155605cac842f9f8515bb5b9614baf2fe0
HANW001 <mosadi@nate.com>
2021-11-01 15:01:22 +0900

    CG-15 카카오 로그인

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-// import React from "react";
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <a href="http://172.30.1.42:5000/oauth"><img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" /> </a>
-//     </div>
-//   );
-// }
-
 import React from "react";
-import KaKaoLogin from 'react-kakao-login';
-
-// const token = "592b62707a03c5616e19cdbbf985a42b";
+// import KaKaoLogin from 'react-kakao-login';
 
 function App() {
   return (
     <div className="App">
-      <h1>'KaKaoLogin'</h1>
+      <h1>KaKaoLogin</h1>
       <a href="http://localhost:5000/oauth"><img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" /> </a>
     </div>
   );
