80156aa1e7636cac83917254a5da0c0ba36a5818
hanw001 <mosadi@nate.com>
2021-11-03 01:19:57 +0900

    add 메인 랜딩 페이지

--- src/main_landing/landing.js
+++ src/main_landing/landing.js
@@ -1,30 +1,44 @@
 import React from "react";
+import './App.css';
 // import KaKaoLogin from 'react-kakao-login';
 
-function Landing() {
-  return (
-    <div className="Landing">
-      <h1>KaKaoLogin</h1>
-      <a href="http://localhost:5000/oauth"><img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" /> </a>
-    </div>
+
+class Landing extends React.Component{
+  
+
+render(){
+  const kakaoname = '김한울';
+  const giftname = '비공개';
+  const gifttarget = '친구';
+  const giftst = '생일선물 / 일반적인 선물'
+  const age = '25~31세';
+  const sex = '여성';
+  const price = '20만원 내외';
+  const like = '화장품'
+    return (  
+      <div >
+        <img className="img" src="img/beo_logo.png" /> 
+        <p className='title'>{kakaoname}님을 위한 선물 제안서</p>
+        <p className='title2'>200백만개의 상품 데이터베이스에서 {kakaoname}님에게 가장 잘 맞는 선물을 찾아보았어요. 광고 없이 오직 당신의 만족만을 생각하고 제안합니다</p>
+        <div className='bgcolor' />
+        <p className='commentName'>For.{giftname}</p>
+        <p className='comment'>선물대상</p>
+        <p className='comment2'>{gifttarget}</p>
+        <p className='comment3'>선물상황</p>
+        <p className='comment4'>{giftst}</p>
+        <p className='comment5'>나이</p>
+        <p className='comment6'>{age}</p>
+        <p className='comment7'>성별</p>
+        <p className='comment8'>{sex}</p>
+        <p className='comment9'>선물예산</p>
+        <p className='comment10'>{price}</p>
+        <p className='comment11'>관심사</p>
+        <p className='comment12'>{like}</p>
+        <img className="commentimg" src="img/beo_logo.png" /> 
+      </div>
   );
 }
 
-// function KakaoLoginButton() {
-//   return (
-//     <div>
-//       <KaKaoLogin
-//         // rest api 키가 아닌 js 키를 사용해야 합니다.
-//         jsKey={"592b62707a03c5616e19cdbbf985a42b"}
-//         onSuccess={(res) => console.log(res)}
-//         onFailure={(res) => console.log(res)}
-        
-//         // getPofile 속성을 주지 않으면 유저 정보를 받을 수 없습니다.
-//         getProfile={true}
-//       >
-//       </KaKaoLogin>
-//     </div>
-//   );
-// }
+}
 
 export default Landing;
